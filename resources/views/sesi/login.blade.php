dd2dbc031f9f55b597011186d2aa33152b28eb5a
Drakkura <gonhgyhjj@student.telkomuniversity.ac.id>
2024-04-21 11:01:01 +0700

    Perbaikan fitur login

--- resources/views/sesi/login.blade.php
+++ resources/views/sesi/login.blade.php
@@ -39,7 +39,7 @@
                         </div>
                     @endif
                     @csrf
-                    //login
+                    //login perbaikan
                     <h3 class="mb-3 text-center">Welcome back </h3>
                     <h4 class="text-center">Login</h4>
                     <div class="mb-3">
